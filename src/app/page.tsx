ac599093618feccd96b3c2f9ae98e2815cd8ffa4
Youngtechie <olaegbeolamilekan1@gmail.com>
2024-01-14 13:18:17 +0100

    [update]::

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,22 @@
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from "../components";
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Olaegbe's Portfolio",
+  description: 'Passionate frontend developer dedicated to transforming concepts into visually appealing and seamlessly functional websites.',
+  keywords: ['Olaegbe Abdul-Rahmon', 'youngtechie'],
+  creator: 'Olaegbe Abdul-Rahmon',
+  openGraph: {
+    type: 'website',
+    url: 'https://oa-3dportfolio.netlify.app/',
+    title: "Olaegbe's Portfolio",
+    description: 'Passionate frontend developer dedicated to transforming concepts into visually appealing and seamlessly functional websites.',
+    images: '/ogImage.png'
+  },
+  icons: {
+    icon: [{ url: '/logo.svg', sizes: 'any', type: 'image/svg+xml' }]
+  }
+}
 
 export default function Home() {
   return (
