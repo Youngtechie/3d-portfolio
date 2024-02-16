c587fcaaae9cd98a371d08183a97a875ff52d651
Youngtechie <olaegbeolamilekan1@gmail.com>
2024-02-16 08:27:48 +0100

    change...

--- src/constants/style.js
+++ src/constants/style.js
@@ -4,12 +4,12 @@ const styles = {
   padding: "sm:px-8 px-3 sm:py-8 py-5",
 
   heroHeadText:
-    "font-black text-white lg:text-[40px] sm:text-[40px] xs:text-[30px] text-[20px] lg:leading-[65px] mt-1",
+    "font-black text-black lg:text-[40px] sm:text-[40px] xs:text-[30px] text-[20px] lg:leading-[65px] mt-1",
   heroSubText:
-    "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[30px] xs:text-[20px] text-[20px] lg:leading-[45px]",
+    "text-black font-medium lg:text-[30px] sm:text-[30px] xs:text-[20px] text-[20px] lg:leading-[45px]",
 
   sectionHeadText:
-    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+    "text-black font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
   sectionSubText:
     "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
 };
